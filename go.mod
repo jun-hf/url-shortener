e2a99de7d7aa0746193b557c0b6c71f8ea32077d
jun-hf <86782267+jun-hf@users.noreply.github.com>
2024-03-25 13:43:49 +0000

    created yaml handler

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jun-hf/url-shortener
 
 go 1.22.1
+
+require gopkg.in/yaml.v2 v2.4.0 // indirect
--- handler/handler.go
+++ handler/handler.go
@@ -1,6 +1,10 @@
 package handler
 
-import "net/http"
+import (
+	"net/http"
+
+	yaml "gopkg.in/yaml.v2"
+)
 
 func MapHandler(urlsToPaths map[string]string, fallback http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -14,5 +18,21 @@ func MapHandler(urlsToPaths map[string]string, fallback http.Handler) http.Handl
 }
 
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	return nil, nil
+	var urlsPath []UrlsPath
+	err := yaml.Unmarshal(yml, &urlsPath)
+	if err != nil {
+		return nil, err
+	}
+
+	pathsUrlMap := make(map[string]string)
+	for _, pu := range(urlsPath) {
+		pathsUrlMap[pu.Path] = pu.Url
+	}
+
+	return MapHandler(pathsUrlMap, fallback), nil
+}
+
+type UrlsPath struct {
+	Path string `yaml: "path"`
+	Url string `yaml:"url"`
 }
